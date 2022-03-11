834a349e4b19b77c4ab6a1be1ca377e1c88113d9
Daroshi11260 <rytaneja@gmail.com>
2022-03-11 15:28:21 -0800

    Testing out some dumpy stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/YourMotherDrove.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.teleops;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Teleop
+public class YourMotherDrove extends LinearOpMode {
+
+    @Override
+    public void start(){ super.start(); }
+
+    @Override
+    public void loop() {
+        super.loop();
+    }
+}
